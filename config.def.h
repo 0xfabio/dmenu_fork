ca7d12f10c7f88bdbb4d7342639062d876a7fda5
Fabio <deinewebsiteberlin@gmail.com>
2020-12-22 16:03:23 +0100

    Idontknmow

--- config.def.h
+++ config.def.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ffb86c", "#282a36" },
-	[SchemeSel] = { "#f1fa8c", "#f8f8f2" },
-	[SchemeOut] = { "#6272a4", "#282a36 },
+	[SchemeNorm] = { "#6272a4", "#282a36" },
+	[SchemeSel] = { "#6272a4", "#f8f8f2" },
+	[SchemeOut] = { "#6272a4", "#282a36" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
